d8836c20876b585e23965958e24fcf9a620c7e58
rafael <rafaelsistem87@gmail.com>
2021-12-03 01:09:00 -0700

    details 2

--- build/precache-manifest.d5deb5f28ac154f65051cc5047942a3e.js
+++ build/precache-manifest.d5deb5f28ac154f65051cc5047942a3e.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "bf14f1ebb5a0d1fa06aed7c60651660e",
+    "revision": "05a36e3615e172882b5d95eeea6273f5",
     "url": "/lead/index.html"
   },
   {
-    "revision": "a95ad76ae31730d91684",
+    "revision": "47ff5c4c4c42ce66d161",
     "url": "/lead/static/css/main.ae165b20.chunk.css"
   },
   {
-    "revision": "2475bebd414081f20baf",
-    "url": "/lead/static/js/2.48029c45.chunk.js"
+    "revision": "09e62229d7f5be85784c",
+    "url": "/lead/static/js/2.0bf6504b.chunk.js"
   },
   {
     "revision": "886fe10c5a843349e5177c678e7a2f01",
-    "url": "/lead/static/js/2.48029c45.chunk.js.LICENSE.txt"
+    "url": "/lead/static/js/2.0bf6504b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a95ad76ae31730d91684",
-    "url": "/lead/static/js/main.cfbae4ad.chunk.js"
+    "revision": "47ff5c4c4c42ce66d161",
+    "url": "/lead/static/js/main.8d9831f7.chunk.js"
   },
   {
     "revision": "a906f5cad842ad417986",
