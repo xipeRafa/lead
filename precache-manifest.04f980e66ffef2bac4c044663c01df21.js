cfc6748863055109fa7f5a46060b9bb1cf323223
rafael <rafaelsistem87@gmail.com>
2021-12-11 22:17:18 -0700

    Updates

---
+++ precache-manifest.04f980e66ffef2bac4c044663c01df21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a098ce10627ab4eb8df3dce9fd576cda",
+    "url": "/lead/index.html"
+  },
+  {
+    "revision": "6d0aa93a315e323c7c18",
+    "url": "/lead/static/css/main.ae165b20.chunk.css"
+  },
+  {
+    "revision": "c9486c4316e98b880b55",
+    "url": "/lead/static/js/2.56bdf640.chunk.js"
+  },
+  {
+    "revision": "886fe10c5a843349e5177c678e7a2f01",
+    "url": "/lead/static/js/2.56bdf640.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d0aa93a315e323c7c18",
+    "url": "/lead/static/js/main.abbcb503.chunk.js"
+  },
+  {
+    "revision": "a906f5cad842ad417986",
+    "url": "/lead/static/js/runtime-main.8ab801f2.js"
+  },
+  {
+    "revision": "5eff140822e6799b93adc8d462ac9366",
+    "url": "/lead/static/media/bannerGym.5eff1408.png"
+  }
+]);
